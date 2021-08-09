bf954643d4e4b9c74941080fe624d32c9688a419
thais de rodez <thaisderodez@gmail.com>
2021-08-09 14:33:54 +0200

    ruby style correction

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,14 @@
 class QuestionsController < ApplicationController
 
-  def ask
-  end
+  def ask; end
 
   def answer
-    return 'Great!' if params[:question] == 'I am going to work'
-    return 'Silly question, get dressed and go to work!' if params[:question].include?('?')
-
-    'I don\'t care, get dressed and go to work!'
+    @answer = if params[:question].downcase == 'i am going to work'
+                'Great!'
+              elsif params[:question].include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
   end
-  helper_method :answer
 end
